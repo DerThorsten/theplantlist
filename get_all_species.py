76969c9dae24fcb7d39e29db8b19cbd38b6f2fba
Dmitry E. Kislov <kislov@easydan.com>
2016-12-09 20:37:02 +1000

    Fist commit

---
+++ get_all_species.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib2
+from subprocess import call
+
+baseurl = 'http://www.theplantlist.org/'
+
+f = urllib2.urlopen(baseurl + '1.1/browse/-/')
+content = f.read()
+
+soup = BeautifulSoup(content, 'html.parser')
+
+for item in soup.find('ul', id='nametree').find_all('li'):
+    fname = item.a.i.text + '.csv'
+    url = item.a['href']
+    call(['wget', baseurl + url + fname])
+
+
