c12a2759c0e7fb9b99f41ea720fab318d1d415fe
Dmitry E. Kislov <kislov@easydan.com>
2016-12-09 21:57:09 +1000

    All data was downloaded: 88MB approx

--- get_all_species.py
+++ get_all_species.py
@@ -1,14 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib2
 from subprocess import call
-
 baseurl = 'http://www.theplantlist.org/'
-
 f = urllib2.urlopen(baseurl + '1.1/browse/-/')
 content = f.read()
-
 soup = BeautifulSoup(content, 'html.parser')
-
 for item in soup.find('ul', id='nametree').find_all('li'):
     fname = item.a.i.text + '.csv'
     url = item.a['href']
